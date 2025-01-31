755630984a5f918a3b8280a1d8320b39e519e18b
OliverCR14 <oliver.peart.arroyo@outlook.com>
2025-01-30 22:47:14 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+function doSomething () {
+	
+}
 let Goku = sprites.create(assets.image`goku`, SpriteKind.Player)
 controller.moveSprite(Goku, 100, 100)
 let goku_battle_form = sprites.create(assets.image`goku battle form`, SpriteKind.Player)
